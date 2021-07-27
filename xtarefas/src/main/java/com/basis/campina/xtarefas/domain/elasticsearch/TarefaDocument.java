b3613f6f17205bb9ecd6c9f1e9a089d54e2e409c
Thalys Menezes Cunha Gadelha <thalys.gadelha@basis.com.br>
2021-07-27 14:48:26 -0300

    Front added

--- xtarefas/src/main/java/com/basis/campina/xtarefas/domain/elasticsearch/TarefaDocument.java
+++ xtarefas/src/main/java/com/basis/campina/xtarefas/domain/elasticsearch/TarefaDocument.java
@@ -40,20 +40,20 @@ public class TarefaDocument extends BaseDocument {
                     format = DateFormat.custom, pattern = DATE_PATTERN)})
     private String dataConclusao;
 
-    @MultiField(mainField = @Field(type = FieldType.Integer, store = true),
-            otherFields = {@InnerField(suffix = SORT, type = FieldType.Integer, store = true)})
-    private int status;
+    @MultiField(mainField = @Field(type = FieldType.Text, store = true),
+            otherFields = {@InnerField(suffix = SORT, type = FieldType.Text, store = true)})
+    private String status;
 
-    @MultiField(mainField = @Field(type = FieldType.Long, store = true),
-            otherFields = {@InnerField(suffix = SORT, type = FieldType.Long, store = true)})
-    private  Long idResponsavel;
+    @MultiField(mainField = @Field(type = FieldType.Text, store = true),
+            otherFields = {@InnerField(suffix = SORT, type = FieldType.Text, store = true)})
+    private  String idResponsavel;
 
     public TarefaDocument(Long id, String nome, LocalDate dataInicio, LocalDate dataConclusao, StatusTarefa status, Long idResponsavel) {
         this.id = id;
         this.nome = nome;
         this.dataInicio = dataInicio != null ? dataInicio.format(DateTimeFormatter.ofPattern(DATE_PATTERN)) : null;
         this.dataConclusao = dataConclusao != null ? dataConclusao.format(DateTimeFormatter.ofPattern(DATE_PATTERN)) : null;
-        this.status = status.ordinal();
-        this.idResponsavel = idResponsavel;
+        this.status = status.toString();
+        this.idResponsavel = idResponsavel.toString();
     }
 }
